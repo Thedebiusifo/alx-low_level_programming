ae2b30147853eb31e8a9e3ac7507b7dfb193e039
Thedebiusifo <Debbieusifo@gmail.com>
2022-09-15 18:16:37 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
+
 int _isupper(int c);
 int _isdigit(int c);
-int _mul(int a, int b);
-int _putchar(char);
+int mul(int a, int b);
+int _putchar(char c);
+void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
